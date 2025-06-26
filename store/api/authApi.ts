6119dc9858ca91b53b68435e9cd09763c0a0b901
Mihir <msonii2000@gmail.com>
2025-06-26 13:34:30 +0530

    env fixes + redirect

--- store/api/authApi.ts
+++ store/api/authApi.ts
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const authApi = createApi({
   reducerPath: 'authApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:8000/api',
+    baseUrl: process.env.NEXT_PUBLIC_API_URL,
   }),
   endpoints: (builder) => ({
     login: builder.mutation<AuthResponse, AuthReqSchema>({
